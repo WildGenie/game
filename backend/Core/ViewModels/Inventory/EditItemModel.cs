5cb0073124bbb5a4ecd12aab8ae135324c41d083
Christian LeVesque <christian@christianlevesque.io>
2021-02-24 21:43:36 -0600

    added Add and Edit itemmodel

---
+++ backend/Core/ViewModels/Inventory/EditItemModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using Core.DataModels.Inventory;
+
+namespace Core.ViewModels.Inventory
+{
+	[ExcludeFromCodeCoverage]
+	public class EditItemModel
+	{
+		public int Id { get; set; }
+		
+		[Required]
+		public string Name { get; set; }
+		
+		[Required]
+		[MaxLength(500)]
+		public string Description { get; set; }
+		public short MaxDurability { get; set; }
+		public byte MaxCharges { get; set; }
+		public ItemType Type { get; set; }
+	}
+}
